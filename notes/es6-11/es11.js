fd1e6dc73c8057b83a1b0b93334efaf66f8db5cb
bxxmi <bmi6638@gmail.com>
2021-08-17 17:04:53 +0900

    feat: Study ES11 (Optional Chaining, Nullish Coalescing Operator)

---
+++ notes/es6-11/es11.js
@@ -0,0 +1,73 @@
+// 1. Optional Chaining (ES11)
+const person1 = {
+  name: 'Bomi',
+  job: {
+    title: 'S/W Enginner',
+    manager: {
+      name: 'Bob',
+    },
+  },
+};
+
+const person2 = {
+  name: 'Bomi'
+};
+
+// e.g. 👎🏻👎🏻👎🏻👎🏻👎🏻👎🏻👎🏻👎🏻👎🏻👎🏻👎🏻
+function printManager(person) {
+  console.log(person.job.manager.name);
+}
+
+printManager(person1);
+
+// e.g. 👎🏻👎🏻👎🏻👎🏻👎🏻
+function printManager(person) {
+  console.log(
+    person.job
+    ? person.job.manager
+      ? person.job.manager.name
+      : undefined
+    : undefined
+  );
+}
+
+printManager(person1);
+
+// e.g. 👎🏻
+function printManager(person) {
+  console.log(person.job && person.job.manager && person.job.manager.name);
+  
+  printManager(person1);
+}
+
+// e.g. 👍🏻
+function printManager(person) {
+  console.log(person.job?.manager?.name);
+}
+
+printManager(person1);
+
+// 2. Nullish Coalescing Operator (ES11)
+// Logical OR operator
+// 📌 false: false, '', 0, null, undefined
+const name = 'Bomi';
+const userName = name || 'Guest';
+console.log(userName);
+
+// e.g. 👎🏻
+const name = '';
+const userName = name || 'Guest';
+console.log(userName); // 결과: Guest
+
+const num = 0;
+const message = num || 'undefined';
+console.log(message);
+
+// e.g. 👍🏻
+const name = '';
+const userName = name ?? 'Guest';
+console.log(userName); // 결과: 입력한 그대로 빈 값 출력
+
+const num = 0;
+const message = num ?? 'undefined';
+console.log(message); // 결과: 입력한 그대로 0 출력
